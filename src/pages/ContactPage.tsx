023f6e33746a4f6db26566092bac1d45d5436904
Raúl López Penalva <raullopezpenalva@icloud.com>
2026-02-14 17:58:03 +0100

    Feature: Enhance Contact Page with LinkedIn and GitHub buttons, and update styles

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,10 +1,27 @@
+import Section from "../components/ui-patterns/Section";
+import Button from "../components/ui-primitives/Button";
+import GitHubIconLetter from "../components/ui-primitives/icons/GitHubIconLetter";
+import Stack from "../components/ui-primitives/Stack";
+import Text from "../components/ui-primitives/Text";
 
 const ContactPage: React.FC = () => {
     return (
-        <div>
-            <h1>Contact Me</h1>
-            <p>If you'd like to get in touch, please email me at</p>
-        </div>
+        <Section
+            id="contact"
+            title="Contacto"
+            subtitles="Construyamos algo sólido"
+        >
+            <Stack direction="column" gap="lg" justifyContent="flex-start" align="stretch" margin="lg">
+                <Text variant="body">Estoy construyendo mi transición hacia DevOps y arquitectura de distemas. Si crees que puedo aportar en tu equipo o proyecto, estaré encantado de hablar.</Text>
+                <Stack direction="row" gap="md" justifyContent="center" align="center" margin="lg">
+                    <Button variant="primary" to="mailto:raullopezpenalva@icloud.com">Enviar correo</Button>
+                    <Button variant="alternative" to="https://www.linkedin.com/in/raullopezpenalva/">LinkedIn</Button>
+                    <Button variant="secondary" to="https://github.com/raullopezpenalva">
+                        <GitHubIconLetter />
+                    </Button>
+                </Stack>
+            </Stack>
+        </Section>
     );
 };
 
